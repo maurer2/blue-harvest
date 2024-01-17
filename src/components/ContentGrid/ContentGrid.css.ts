a0677a8d8d5c2e824c264506933d4979139c5169
Martin <mr.mmaurer@gmail.com>
2024-01-17 23:53:55 +0000

    Added counter to boxes

--- src/components/ContentGrid/ContentGrid.css.ts
+++ src/components/ContentGrid/ContentGrid.css.ts
@@ -1,6 +1,7 @@
 import { style } from '@vanilla-extract/css';
 
 export const grid = style({
+  counterReset: 'box',
   display: 'grid',
   gap: '2rem',
   gridTemplateColumns: 'repeat(auto-fill, minmax(350px, 1fr))',
@@ -14,9 +15,25 @@ export const gridTitle = style({
 export const gridBox = style({
   background: 'var(--murrey)',
   color: 'var(--misty-rose)',
+  counterIncrement: 'box',
   display: 'grid',
   gap: '1rem',
   gridTemplateColumns: 'min-content 1fr',
   margin: 0,
   padding: '1rem',
+  position: 'relative',
+
+  // eslint-disable-next-line perfectionist/sort-objects
+  '::before': {
+    color: 'var(--eerie-black)',
+    content: 'counter(box)',
+    fontSize: '5rem',
+    lineHeight: 0.75,
+    opacity: 0.2,
+    paddingRight: 'inherit',
+    paddingTop: 'inherit',
+    position: 'absolute',
+    right: 0,
+    top: 0,
+  },
 });
